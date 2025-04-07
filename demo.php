0cc230b14fe6a175b9600432c84f95f3b821c9fd
Yllka Fejzullahu <yllka.fejzullahu@student.uni-pr.edu>
2025-04-07 20:17:24 +0200

    prove

--- demo.php
+++ demo.php
@@ -2,6 +2,7 @@
 //yllka
 //variablat globale: $_GET — për të marrë të dhëna nga URL query string, $_POST — për të marrë të dhëna nga forma, $_SESSION — për të ruajtur të dhëna midis faqeve (si përdoruesi që është loguar).,
 // $_SERVER — për informata rreth serverit dhe kërkesës, $_COOKIE — për të lexuar cookie-t, $_FILES — për menaxhimin e file upload.
+echo "HELLO FROM XAMPP!<br>";
 
 $lokacioni = $_GET['lokacion'] ?? 'prishtina';
 $lloji = $_GET['lloji'] ?? 'apartment';
