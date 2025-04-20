b94cfc2d8b9b02db4ff4d94ae7c9cc6b85d1f494
Viola Resyli <violaresyli@gmail.com>
2025-04-20 13:29:03 +0200

    delete

--- aboutUs.php
+++ aboutUs.php
@@ -150,7 +150,7 @@ $teamMembers = [
 				<!--hyperlinks brenda HTML faqes-->
 				<li><a href="aboutUs.php">About Us</a></li>
 				<li><a href="indexYllka.php">Listings</a></li>
-				<li><a href="indexKimete.html">Contact Us</a></li>
+				<li><a href="indexKimete.php">Contact Us</a></li>
 				<li><a href="indexRudina.php">Blog</a></li>
 			</ul>
 		</div>
