900d60d9735e89a2bd23fc24f6d83268c5fa7103
RudinaBulliqi <rudinabulliqi@email.com>
2025-05-25 22:11:56 +0200

    Perdorimini AJAX per lexim dhe update nga nje PHP Skripte.

--- update-views.php
+++ update-views.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+header('Content-Type: application/json'); // për të kthyer JSON
+
 $postId = $_GET['post_id'] ?? '';
 
 if (!empty($postId)) {
@@ -7,6 +9,11 @@ if (!empty($postId)) {
         $_SESSION['post_views'][$postId] = 0;
     }
     $_SESSION['post_views'][$postId]++;
-    echo "Viewed";
+
+    // kthen view count si JSON
+    echo json_encode([
+        'views' => $_SESSION['post_views'][$postId]
+    ]);
+} else {
+    echo json_encode(['error' => 'No post ID']);
 }
-?>
