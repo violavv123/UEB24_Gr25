c11baf29a2bfabd359ba62b3eb1d66ad626fcae6
Yllka Fejzullahu <yllka.fejzullahu@student.uni-pr.edu>
2025-05-25 16:38:25 +0200

    shtimi i try throw catch ne konektimin e databazes (conn.php)

--- conn.php
+++ conn.php
@@ -1,11 +1,17 @@
 <?php
 $user = "root";
 $pass ="";
+
+try{
 $conn = mysqli_connect("localhost",$user,$pass,"real_estate");
+
 if(!$conn){
-    echo "<br> Gabim gjate qasjes ne databaze me metoden mysqli!".mysqli_connect_error();
+    throw new Exception ("Gabim gjate qasjes ne databaze me metoden mysqli!" . mysqli_connect_error());
+    }   
+        echo "<br>Komunikimi me databazë me metodën mysqli u krye me sukses!";
+
+} catch (Exception $e){
+ echo "<br>Gabim: " . $e->getMessage();
     die();
-}else{
-    echo"<br> Komunikimi me databaze me metoden mysqli u krye me sukses!";
 }
 ?>
